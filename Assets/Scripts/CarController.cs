67d1412059306ad1c97e7e6e958a1368205f0e71
Thomas Sandelin <thomas.sandelin@gmail.com>
2020-11-05 11:27:51 +0200

    added rded random car spawnpoints

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    private GameObject Car;
+    GameObject[] ParkingSpots;
+
     private const string HORIZONTAL = "Horizontal";
     private const string VERTICAL = "Vertical";
-
     private float horizontalInput;
     private float verticalInput;
     private float currentSteerAngle;
@@ -28,8 +30,14 @@ public class CarController : MonoBehaviour
     [SerializeField] private Transform rearLeftWheelTransform;
     [SerializeField] private Transform rearRightWheelTransform;
 
-    // TODO if car.rotationX == -180/180 eller   (x > 100 || x < -100) --> Restart()
+    // TODO if carflipsover -> ResetCar();
 
+    private void Start()
+    {
+        ParkingSpots = GameObject.FindGameObjectsWithTag("ParkingSpot");
+        Car = GameObject.Find("Car");
+        Car.GetComponent<Rigidbody>().centerOfMass = new Vector3(0, 0.3f, 0);
+    }
 
     private void FixedUpdate()
     {
@@ -37,6 +45,12 @@ public class CarController : MonoBehaviour
         HandleMotor();
         HandleSteering();
         UpdateWheels();
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            ResetCar();
+        }
+
     }
 
 
@@ -84,5 +98,13 @@ public class CarController : MonoBehaviour
         wheelCollider.GetWorldPose(out Vector3 pos, out Quaternion rot);
         wheelTransform.rotation = rot;
         wheelTransform.position = pos;
+    }
+
+    public void ResetCar()
+    {
+        Vector3 position = new Vector3(UnityEngine.Random.Range(-230f, -235f), 0, UnityEngine.Random.Range(0f, 40f));
+
+        frontLeftWheelCollider.attachedRigidbody.velocity = new Vector3(0, 0, 0);
+        Car.transform.SetPositionAndRotation(position, new Quaternion(0, 0, 0, 0));
     }
 }

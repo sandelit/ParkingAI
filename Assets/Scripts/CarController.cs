cefcf697c4b26bcd50fe81493b34a403531db9b9
Thomas Sandelin <thomas.sandelin@gmail.com>
2020-11-05 15:44:58 +0200

    collect observations

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
 using UnityEngine;
 
-public class CarController : MonoBehaviour
+public class CarController : Agent
 {
     private GameObject Car;
     GameObject[] ParkingSpots;
@@ -15,6 +17,8 @@ public class CarController : MonoBehaviour
     private float currentSteerAngle;
     private float currentbreakForce;
     private bool isBreaking;
+
+    Rigidbody rb;
 
     [SerializeField] private float motorForce;
     [SerializeField] private float breakForce;
@@ -30,17 +34,57 @@ public class CarController : MonoBehaviour
     [SerializeField] private Transform rearLeftWheelTransform;
     [SerializeField] private Transform rearRightWheelTransform;
 
-    // TODO if carflipsover -> ResetCar();
 
-    private void Start()
+    public override void Initialize()
     {
         ParkingSpots = GameObject.FindGameObjectsWithTag("ParkingSpot");
         Car = GameObject.Find("Car");
-        Car.GetComponent<Rigidbody>().centerOfMass = new Vector3(0, 0.3f, 0);
+        rb = Car.GetComponent<Rigidbody>();
+        rb.centerOfMass = new Vector3(0, 0.3f, 0);
     }
+
+    public override void OnEpisodeBegin()
+    {
+        ResetCar();
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        /*
+        _controller.CurrentSteeringAngle = vectorAction[0];
+        _controller.CurrentAcceleration = vectorAction[1];
+        _controller.CurrentBrakeTorque = vectorAction[2];
+        */
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        Vector3 dirToTarget = (ParkingSpots[0].transform.position - transform.position).normalized;
+
+        sensor.AddObservation(transform.position.normalized); // Bilens position
+        sensor.AddObservation(transform.forward); // Bilens Z-rotation
+        sensor.AddObservation(transform.right); // Bilens X-rotation
+        sensor.AddObservation(rb.velocity); // Bilens hastighet ??????? kanske idk ????
+        sensor.AddObservation(ParkingSpots[0].transform.position); // Parkeringens position
+        sensor.AddObservation(dirToTarget); // Riktning mot parkering
+
+    }
+
 
     private void FixedUpdate()
     {
+        RequestDecision();
+
+
         GetInput();
         HandleMotor();
         HandleSteering();
@@ -106,7 +150,7 @@ public class CarController : MonoBehaviour
         Vector3 position = new Vector3(UnityEngine.Random.Range(-230f, -235f), 0, UnityEngine.Random.Range(0f, 40f));
         Vector3 rotation = new Vector3(0, 0, UnityEngine.Random.Range(-1, 1));
 
-        frontLeftWheelCollider.attachedRigidbody.velocity = new Vector3(0, 0, 0);
+        frontLeftWheelCollider.attachedRigidbody.velocity = new Vector3(0f, 0f, 0f);
 
         Car.transform.position = position;
         Car.transform.rotation = Quaternion.LookRotation(rotation);
